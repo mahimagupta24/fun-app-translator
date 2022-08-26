02822015c3e6cb80eaf2b6395d9590ab59c4f549
mahimagupta24 <mahimagupta2401@gmail.com>
2022-08-26 18:38:27 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,4 @@
+var translate = document.querySelector("#btn-txt");
+var txtInput = document.querySelector("#txt-input");
+var oututDiv = document.querySelector("#output");
+var serverURL="https://api.funtranslations.com/translate/shakespeare.json";
